dedbf8d6fd713b9ba53a9f0077dd83606c58e8f4
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-11-10 11:36:52 +0000

    rm local files

--- src/main/java/io/confluent/connect/storage/partitioner/HeaderPartitioner.java
+++ src/main/java/io/confluent/connect/storage/partitioner/HeaderPartitioner.java
@@ -14,89 +14,64 @@
  */
 
 package io.confluent.connect.storage.partitioner;
+
 import org.apache.kafka.common.utils.Utils;
-import org.apache.kafka.connect.data.Schema.Type;
 import org.apache.kafka.connect.header.Header;
 import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.sink.SinkRecord;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-
-import java.util.*;
-
-import io.confluent.connect.storage.common.StorageCommonConfig;
-import io.confluent.connect.storage.errors.PartitionException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class HeaderPartitioner<T> extends DefaultPartitioner<T> {
-    private static final Logger log = LoggerFactory.getLogger(HeaderPartitioner.class);
-    public static final String PARTITION_HEADER_NAME_CONFIG = "partition.header.name";
-  //  public static final String PARTITION_HEADER_FORMATTER_PATTERN= "partition.header.pattern";
-    //  public static final String PARTITION_HEADER_FORMATTER_TimeZone= "partition.header.timezone";
-    //  public static final String PARTITION_HEADER_FORMATTER_TimeZone= "partition.header.locale";
+    private static final Logger LOG = LoggerFactory.getLogger(HeaderPartitioner.class);
 
+    private static final String PARTITION_HEADER_NAME_CONFIG = "partition.header.name";
 
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("'year='yyyy'/month='MM'/day='dd");
 
     private List<String> headerNames;
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("'year='yyyy'/month='MM'/day='dd");
 
-
-
-
-    @SuppressWarnings("unchecked")
     @Override
     public void configure(Map<String, Object> config) {
-        String headersRaw = (String)config.get(PARTITION_HEADER_NAME_CONFIG);
-        log.debug ("headers " + headersRaw);
-        headerNames = (List<String>) Arrays.asList(headersRaw.split(",", -1));
-        delim = (String) config.get(StorageCommonConfig.DIRECTORY_DELIM_CONFIG);
+        String headersRaw = (String) config.get(PARTITION_HEADER_NAME_CONFIG);
+        headerNames = Arrays.asList(headersRaw.split(",", -1));
     }
 
     @Override
     public String encodePartition(SinkRecord sinkRecord) {
-
         Headers headers = sinkRecord.headers();
-        log.debug("Lista total" + headerNames.size());
-
-        for (String s : headerNames) {
-            log.debug(s);
-
-        }
-            StringBuilder builder = new StringBuilder();
-            for (String headerName : headerNames) {
-                if (builder.length() > 0) {
-                    builder.append(this.delim);
-                }
-                log.debug(headerName);
-                Header partitionKey = headers.lastWithName(headerName);
-                if (partitionKey.value() == null ) {
-                    builder.append(headerName + "=" + "");
-                }
-                else{
-                    builder.append(headerName + "=" + partitionKey.value().toString().replace(' ', '_'));
-                    // failure
-            }
-
-            }
+        StringBuilder builder = new StringBuilder();
+        for (String headerName : headerNames) {
+            if (builder.length() > 0) {
                 builder.append(this.delim);
-                builder.append(LocalDate.now().format(formatter));
+            }
+            LOG.debug(headerName);
+            Header partitionKey = headers.lastWithName(headerName);
+            if (partitionKey.value() == null) {
+                builder.append(headerName).append("=");
+            } else {
+                builder.append(headerName).append("=").append(partitionKey.value().toString().replace(' ', '_'));
+                // failure
+            }
 
-
-            return builder.toString();
         }
+        builder.append(this.delim);
+        builder.append(LocalDate.now().format(FORMATTER));
+
+        return builder.toString();
+    }
 
     @Override
     public List<T> partitionFields() {
         if (partitionFields == null) {
-            partitionFields = newSchemaGenerator(config).newPartitionFields(
-                    Utils.join(headerNames, ",")
-            );
+            partitionFields = newSchemaGenerator(config)
+                    .newPartitionFields(Utils.join(headerNames, ","));
         }
         return partitionFields;
     }
